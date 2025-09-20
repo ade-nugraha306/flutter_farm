a9a8a043a3c85e42d2f08db92aa5ca852565c04e
Ade Nugraha <adenuge12@gmail.com>
2025-09-20 20:11:35 +0800

    creating image and row list

--- lib/screen_pertama.dart
+++ lib/screen_pertama.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
-class ScreenPertama extends StatelessWidget {
+class ScreenPertama extends StatefulWidget {
   const ScreenPertama({super.key});
+
+  @override
+  State<ScreenPertama> createState() => _ScreenPertamaState();
+}
+
+class _ScreenPertamaState extends State<ScreenPertama> {
+  DateTime? selectedDate;
+
+  Future<void> _pickDate() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,14 +37,28 @@ class ScreenPertama extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            FadeInImage(
+              placeholder: const AssetImage("assets/images/loading.gif"),
+              image: const AssetImage(
+                "assets/images/rural-farm-landscape-stockcake.jpg",
+              ),
+              fadeInDuration: const Duration(seconds: 1),
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 14),
             // Row dengan 3 column (icon + text)
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
+              children: [
                 // Column 1
                 Column(
                   children: [
-                    Icon(Icons.calendar_today, size: 30),
+                    GestureDetector(
+                      onTap: _pickDate,
+                      child: Icon(Icons.calendar_today, size: 30),
+                    ),
                     SizedBox(height: 8),
                     Text("Open Everyday"),
                   ],
@@ -54,6 +90,47 @@ class ScreenPertama extends StatelessWidget {
               "diterapkan dalam bentuk spot swafoto Instagramable.",
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 14),
+            ),
+            const SizedBox(height: 12),
+            Expanded(
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16),
+                    child: FadeInImage.assetNetwork(
+                      placeholder: "assets/images/loading.gif",
+                      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDAXjbNpA2IwhA6Q_uQUuD45V4Riv0VrpUDg&s",
+                      width: 300,
+                      height: 300,
+                      fit: BoxFit.cover,
+                      fadeInDuration: const Duration(seconds: 1),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16),
+                    child: FadeInImage.assetNetwork(
+                      placeholder: "assets/images/loading.gif",
+                      image: "https://cache.lahelu.com/image-PkkKJ4N1A-71421",
+                      width: 300,
+                      height: 300,
+                      fit: BoxFit.cover,
+                      fadeInDuration: const Duration(seconds: 1),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16),
+                    child: FadeInImage.assetNetwork(
+                      placeholder: "assets/images/loading.gif",
+                      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5I3_fH9jrE2tCs--YYfgYbblvp7XyovVUdA&sap-rocky-net-worth.jpg",
+                      width: 300,
+                      height: 300,
+                      fit: BoxFit.cover,
+                      fadeInDuration: const Duration(seconds: 1),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
